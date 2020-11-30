303315a9752da61de6cd619b657bd992cec551ae
Johan <charger_gt2012@hotmail.com>
2020-11-30 16:29:51 -0600

    App terminada

--- services/socket.js
+++ services/socket.js
@@ -1,12 +1,38 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/Bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Bon Jovi'));
+
+// console.log(bands);
 
 io.on('connection', client=>{
-    console.log('Client conected');
+    console.log('Client connected');
+    client.emit('active-bands', bands.getBands());
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
     client.on('disconnect', () => {
         console.log('Client disconnected');
     });
     client.on('message', (payload) => {
         console.log('message', payload);
-        client.emit('message', { admin: 'Hi there' });
+        // io.emit('message', { admin: 'Hi there' }); // Emite a todos
+        // client.broadcast.emit('message', { admin: 'Hi there' }); // Emite a todos menos al emisor
+        client.broadcast.emit('message', payload);
     });
 });
